ab46ae058565a8416b2ad2a618b1d6618108c0ac
Igor <igorprado@outlook.com>
2017-08-29 11:16:25 -0300

    Index e CSS
    Criada primeira index e linkada ao css

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+	<meta charset="UTF-8">
+	<title>Nivia - Fisioterapia Pélvica em São Paulo</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<h1>Fisioterapia Pélvica</h1>
+</body>
+</html>
